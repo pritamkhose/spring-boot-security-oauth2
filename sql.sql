e275fad3ca59863b87fce9e6d9a9ffb4ff3b986c
Pritam <pritamkhose@gmail.com>
2018-11-20 00:51:43 +0530

    add log4j

--- sql.sql
+++ sql.sql
@@ -1,17 +1,14 @@
 CREATE SCHEMA `oauthtest` ;
 
-INSERT INTO User (id, username, password, salary, age) VALUES (1, 'Alex123', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 3456, 33);
-INSERT INTO User (id, username, password, salary, age) VALUES (2, 'Tom234', '$2a$04$PCIX2hYrve38M7eOcqAbCO9UqjYg7gfFNpKsinAxh99nms9e.8HwK', 7823, 23);
-INSERT INTO User (id, username, password, salary, age) VALUES (3, 'Adam', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 4234, 45);
-
+INSERT INTO user (id, username, password, salary, age) VALUES (1, 'Alex123', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 3456, 33);
+INSERT INTO user (id, username, password, salary, age) VALUES (2, 'Tom234', '$2a$04$PCIX2hYrve38M7eOcqAbCO9UqjYg7gfFNpKsinAxh99nms9e.8HwK', 7823, 23);
+INSERT INTO user (id, username, password, salary, age) VALUES (3, 'Adam', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 4234, 45);
+INSERT INTO user (id, username, password, salary, age) VALUES (4, 'pritam', '$2a$10$corvQdKRrBrI6XBPBb7x.Ovhu5VtKediZ4ogwNcyBLnvHsx9FTmdq', 987894, 27);
 
 SELECT * FROM oauthtest.user;
 
-SELECT * FROM oauthtest.data;
+
 INSERT INTO `oauthtest`.`data` (`id`, `descp`, `phone`, `pin`, `title`) VALUES ('1', 'customercare no', '123', '414001', 'BSNL');
+INSERT INTO `oauthtest`.`data` (`id`, `descp`, `phone`, `pin`, `title`) VALUES ('2', 'customercare no', '123', '414001', 'JIO');
 
-
---1	33	$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu	3456	Alex123
---2	23	$2a$04$PCIX2hYrve38M7eOcqAbCO9UqjYg7gfFNpKsinAxh99nms9e.8HwK	7823	Tom234
---3	45	$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu	4234	Adam
---4	27	$2a$10$corvQdKRrBrI6XBPBb7x.Ovhu5VtKediZ4ogwNcyBLnvHsx9FTmdq	987894	pritam
+SELECT * FROM oauthtest.data;
--- src/main/java/com/devglan/Application.java
+++ src/main/java/com/devglan/Application.java
@@ -1,14 +1,22 @@
 package com.devglan;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+	private static final Logger LOGGER = LogManager.getLogger(Application.class);
+	
 	// https://www.devglan.com/spring-security/spring-boot-security-oauth2-example
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        
+    	LOGGER.info("Info level log message");
+        LOGGER.debug("Debug level log message");
+        LOGGER.error("Error level log message");
     }
 
 }
